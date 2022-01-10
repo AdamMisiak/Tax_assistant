18594cb567f76ddce250136120d828f7e2fd43b9
AdamMisiak <adammisiak3@gmail.com>
2022-01-10 17:46:40 +0100

    Added function to create div dict

--- script.py
+++ script.py
@@ -10,13 +10,20 @@ def open_csv_file():
             rows.append(row[0].replace('"', "").split(';'))
         return rows
 
-# def get_dividends_from_report(report: list):
-#     for div in report:
-#         single_dividend = {}
-#         print(div)
-#         single_dividend['name'] = div[]
+def get_dividends_from_report(report: list):
+    divs_only_report = []
+    for div in report:
+        if div[6] in ['Dividends']:
+            single_dividend = {}
+            single_dividend['name'] = div[1]
+            single_dividend['date'] = div[3]
+            single_dividend['amount'] = div[5]
+            single_dividend['currency'] = div[0]
+            divs_only_report.append(single_dividend)
+    return divs_only_report
 
 
 if __name__ == '__main__':
     report = open_csv_file()
-    # get_dividends_from_report(report)
+    divs_only_report = get_dividends_from_report(report)
+    print(divs_only_report)
