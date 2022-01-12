28e5b8aeb84f2e704f02fb9e00a79670b6ecd35d
AdamMisiak <adammisiak3@gmail.com>
2022-01-12 17:13:42 +0100

    Added function to get previous day

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import csv
 import settings
+import requests
+from datetime import datetime, timedelta
 
 def open_csv_file():
     rows = []
@@ -22,14 +24,39 @@ def get_dividends_from_report(report: list):
             divs_only_report.append(single_dividend)
     return divs_only_report
 
-def calculate_tax_to_pay(dividends_report):
+def get_previous_day_from_date(date: str):
+    year = date[:4]
+    month = date[4:6]
+    day = date[6:8]
+    date_in_string_format = f"{day}-{month}-{year}"
+    date_in_datetime_format = datetime.strptime(date_in_string_format, '%d-%m-%Y')
+    result = date_in_datetime_format - timedelta(days=1)
+    return result
+
+def calculate_tax_to_pay(dividends_report: list):
     pass
 
 def get_currency_rate(currency: str, date: str):
     pass
+    # url = settings.URL_BASE + date
+    # response = requests.get(url, {'format': 'api'})
+
+    # while response.status_code == 404: #this day is holiday/weekend, take previous day 
+    #     date_in_datetime_format = date_in_datetime_format - datetime.timedelta(days=1)
+    #     date_in_string_format = date_in_datetime_format.strftime('%Y-%m-%d')
+
+    #     url = settings.URL_BASE + date_in_string_format
+    #     response = requests.get(url, {'format': 'api'})
+
+    # for rate in response.json()[0]['rates']:
+    #     if rate['code'] == currency:
+    #         result = rate['mid']
+    # return result
 
 
 if __name__ == '__main__':
     report = open_csv_file()
     dividends_report = get_dividends_from_report(report)
     print(dividends_report)
+    # get_currency_rate(dividends_report[0]['date'])
+    print(get_previous_day_from_date(dividends_report[0]['date']))
--- settings.py
+++ settings.py
@@ -1,1 +1,2 @@
 DIVIDEND_FILE_CSV = "data/DIVIDENDS.csv"
+URL_BASE = 'https://api.nbp.pl/api/exchangerates/tables/a/'
