12d62584070045ed54053be59655df65eb36d396
AdamMisiak <adammisiak3@gmail.com>
2022-01-18 16:39:23 +0100

    Refactored code

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import csv
 import settings
 import requests
 from datetime import datetime, timedelta
+from typing import Tuple, Union
 
 def open_csv_file():
     rows = []
@@ -12,7 +13,7 @@ def open_csv_file():
             rows.append(row[0].replace('"', "").split(';'))
         return rows
 
-def get_relevant_data_from_report(report: list):
+def get_relevant_data_from_report(report: list) -> Tuple[list, list]:
     divs_only_report = []
     taxes_only_report = []
     for row in report:
@@ -32,8 +33,7 @@ def get_relevant_data_from_report(report: list):
             taxes_only_report.append(record)
     return divs_only_report, taxes_only_report
 
-# TODO: Add type hints to `date`
-def get_previous_day_from_date(date):
+def get_previous_day_from_date(date: Union[str, datetime]) -> datetime:
     if isinstance(date, datetime):
         result = date - timedelta(days=1)
         return result
@@ -56,19 +56,36 @@ def get_previous_day_from_date(date):
         return result
 
 
-def calculate_tax_to_pay(dividends_report: list):
-    pass
+def calculate_tax_to_pay(dividends_report: list, taxes_report: list) -> float:
+    total_tax_to_paid_in_pln = 0
 
-def get_currency_rate(currency: str, date: str):
+    for received_dividend in dividends_report:
+        paid_withholding_tax = next(filter(lambda tax: tax['name'] == received_dividend['name'] and tax['date'] == received_dividend['date'], taxes_report))
+        if received_dividend['currency'] != settings.PLN_CURRENCY:
+            tax_rate = 0.19
+            if received_dividend['name'] in settings.MLP_STOCKS:
+                # 37% + 4%
+                tax_rate = 0.41
+
+            previous_date = get_previous_day_from_date(received_dividend['date'])
+            currency_rate = get_currency_rate_for_date(received_dividend['currency'], previous_date)
+            received_dividend_in_pln = round(float(received_dividend['amount']) * currency_rate, 2)
+            paid_withholding_tax_in_pln = round(float(paid_withholding_tax['amount']) * currency_rate * -1, 2)
+            tax_to_paid_in_pln = round(tax_rate * received_dividend_in_pln - paid_withholding_tax_in_pln, 2)
+            total_tax_to_paid_in_pln += tax_to_paid_in_pln
+
+    return total_tax_to_paid_in_pln
+
+def get_currency_rate_for_date(currency: str, date: str) -> float:
     date = date.strftime('%Y-%m-%d')
     url = settings.URL_BASE + date
     response = requests.get(url, {'format': 'api'})
-    while response.status_code == 404: #this day is holiday/weekend, take previous day 
+    # this day is holiday/weekend, take previous day 
+    while response.status_code == 404:
         date = get_previous_day_from_date(date)
         date = date.strftime('%Y-%m-%d')
         url = settings.URL_BASE + date
         response = requests.get(url, {'format': 'api'})
-        # get_currency_rate(currency, previous_date)
     for rate in response.json()[0]['rates']:
         if rate['code'] == currency:
             result = rate['mid']
@@ -78,22 +95,5 @@ def get_currency_rate(currency: str, date: str):
 if __name__ == '__main__':
     report = open_csv_file()
     dividends_report, taxes_report = get_relevant_data_from_report(report)
-    total_tax_to_paid_in_pln = 0
-    for received_dividend in dividends_report:
-        print(received_dividend)
-        paid_withholding_tax = next(filter(lambda tax: tax['name'] == received_dividend['name'] and tax['date'] == received_dividend['date'], taxes_report))
-        print(paid_withholding_tax)
-        previous_date = get_previous_day_from_date(received_dividend['date'])
-        if received_dividend['currency'] != settings.PLN_CURRENCY:
-            tax_rate = 0.19
-            if received_dividend['name'] in settings.MLP_STOCKS:
-                tax_rate = 0.41
-            currency_rate = get_currency_rate(received_dividend['currency'], previous_date)
-            received_dividend_in_pln = round(float(received_dividend['amount']) * currency_rate, 2)
-            print(received_dividend_in_pln)
-            paid_withholding_tax_in_pln = round(float(paid_withholding_tax['amount']) * currency_rate * -1, 2)
-            print(paid_withholding_tax_in_pln)
-            tax_to_paid_in_pln = round(tax_rate * received_dividend_in_pln - paid_withholding_tax_in_pln, 2)
-            print(tax_to_paid_in_pln)
-            total_tax_to_paid_in_pln += tax_to_paid_in_pln
+    total_tax_to_paid_in_pln = calculate_tax_to_pay(dividends_report, taxes_report)
     print(total_tax_to_paid_in_pln)
