8c5e1b544553ec49d23030626f6c467345c30ac9
AdamMisiak <adammisiak3@gmail.com>
2022-02-11 17:23:35 +0100

    Added options file

--- options.py
+++ options.py
@@ -6,6 +6,27 @@ from datetime import datetime, timedelta
 from typing import Tuple, Union
 from utils import get_previous_day_from_date
 
+
+
+
+def open_csv_file(file):
+    rows = []
+    with open(file, "r") as file:
+        csvreader = csv.reader(file)
+        header = next(csvreader)
+        for row in csvreader:
+            if row[0] != settings.HEADERS_OF_CSV_FILE:
+                rows.append(row[0].replace('"', "").split("|"))
+        return rows
+
+def merge_csv_files():
+    rows = []
+    files = list(filter(lambda file: file.startswith('STOCKS'), os.listdir("data")))
+    for file in files:
+        rows += open_csv_file(f"data/{file}")
+    return rows    
+    
+
 def get_relevant_data_from_report(report: list) -> list:
     options_report = []
     id = 0
@@ -22,3 +43,13 @@ def get_relevant_data_from_report(report: list) -> list:
             options_report.append(record)
         id += 1
     return options_report
+
+if __name__ == "__main__":
+    report = merge_csv_files()
+    options_report = get_relevant_data_from_report(report)
+    options_report.sort(key=lambda row: row['date'])
+    total_tax_to_paid_in_pln = 0
+
+    for transaction in options_report:
+        print(transaction)
+    
