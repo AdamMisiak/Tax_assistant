0d50468dd2b9f2f4747bc8b6154fc0af5b6f6fd0
AdamMisiak <adammisiak3@gmail.com>
2022-03-10 16:35:22 +0100

    Refactored options handler

--- options.py
+++ options.py
@@ -1,10 +1,9 @@
 import csv
 import os
 import settings
-import requests
-from datetime import datetime, timedelta
-from typing import Tuple, Union
+from datetime import datetime
 from utils import get_previous_day_from_date, get_currency_rate_for_date
+
 
 def open_csv_file(file):
     rows = []
@@ -16,13 +15,14 @@ def open_csv_file(file):
                 rows.append(row[0].replace('"', "").split("|"))
         return rows
 
+
 def merge_csv_files():
     rows = []
-    files = list(filter(lambda file: file.startswith('STOCKS'), os.listdir("data")))
+    files = list(filter(lambda file: file.startswith("STOCKS"), os.listdir("data")))
     for file in files:
         rows += open_csv_file(f"data/{file}")
-    return rows    
-    
+    return rows
+
 
 def get_relevant_data_from_report(report: list) -> list:
     options_report = []
@@ -36,38 +36,35 @@ def get_relevant_data_from_report(report: list) -> list:
             record["amount"] = float(row[4])
             record["currency"] = row[0]
             record["price"] = round(float(row[5]), 2)
-            record["value"] = round(float(row[6]), 2)
+            record["value_usd"] = round(float(row[6]), 2)
+            record["currency_rate_d_1"] = get_currency_rate_for_date(record["currency"], get_previous_day_from_date(record["date"]))
+            record["value_pln"] = round(record["value_usd"]*record["currency_rate_d_1"], 2)
             options_report.append(record)
         id += 1
     return options_report
 
+
 def calculate_tax_to_pay(options_report: list) -> float:
     total_tax_to_paid_in_pln = 0
+    total = 0
 
+    print("OPTIONS:")
+    print("--" * 50)
 
-    print('OPTIONS:')
-    print('--'*50)
+    for option in options_report:
+        if option['amount'] < 0:
+            tax_to_paid_in_pln = round(0.19 * option['value_pln'], 2)
+            total_tax_to_paid_in_pln += tax_to_paid_in_pln
+            total += option['value_pln']
+            print(option)
+            print(total)
+            # TODO: check option which was sold and then bought again - only some values in sold ones - example 2021 BYSI stocks
 
-    for received_options in options_report:
-        previous_date = get_previous_day_from_date(received_options["date"])
-        currency_rate = get_currency_rate_for_date(
-            received_options["currency"], previous_date
-        )
-        received_option_in_pln = round(
-            received_options["price"] * 100 * currency_rate, 2
-        )
-        tax_to_paid_in_pln = round(
-            0.19 * received_option_in_pln, 2
-        )
-        total_tax_to_paid_in_pln += tax_to_paid_in_pln
+    return round(total_tax_to_paid_in_pln, 2)
 
-        print(received_options)
-
-    return total_tax_to_paid_in_pln
 
 def get_summary_options_tax():
     report = merge_csv_files()
     options_report = get_relevant_data_from_report(report)
     total_tax_to_paid_in_pln = calculate_tax_to_pay(options_report)
     return total_tax_to_paid_in_pln
-
