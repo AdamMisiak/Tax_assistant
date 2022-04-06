48c1e0f71d54a23cba23c5af7458f157aece1744
AdamMisiak <adammisiak3@gmail.com>
2022-04-06 17:57:11 +0200

    Added more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,3 +7,13 @@ class TestUtils(TestCase):
         path_to_file = "tests/data"
         utils.get_csv_file_with_rates("2021", path_to_file)
         self.assertTrue(exists(path_to_file))
+
+    def test_fetch_data_from_csv_rates_file(self):
+        rows = utils.get_data_from_csv_file_with_rates("2021")
+        first_row = rows[0]
+        self.assertTrue(isinstance(rows, list))
+        self.assertTrue('USD' in first_row)
+        self.assertTrue('EUR' in first_row)
+        self.assertTrue('GBP' in first_row)
+        self.assertTrue('CAD' in first_row)
+        # add all of them
