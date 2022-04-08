4de4535b0a47bd7e5f7bea114f05c9cf71e29b8b
AdamMisiak <adammisiak3@gmail.com>
2022-04-08 16:07:19 +0200

    Added more utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from datetime import datetime, timedelta
 import utils
 from os.path import exists
 
@@ -12,8 +13,19 @@ class TestUtils(TestCase):
         rows = utils.get_data_from_csv_file_with_rates("2021")
         first_row = rows[0]
         self.assertTrue(isinstance(rows, list))
+        self.assertTrue('date' in first_row)
         self.assertTrue('USD' in first_row)
         self.assertTrue('EUR' in first_row)
         self.assertTrue('GBP' in first_row)
         self.assertTrue('CAD' in first_row)
-        # add all of them
+        self.assertTrue('AUD' in first_row)
+        self.assertTrue('HKD' in first_row)
+        self.assertTrue('CHF' in first_row)
+        self.assertTrue('RUB' in first_row)
+        self.assertTrue('CNY' in first_row)
+
+    def test_get_previous_date(self):
+        current_date = datetime.now()
+        previous_date = utils.get_previous_day_from_date(current_date)
+        self.assertEqual(previous_date, current_date - timedelta(days=1))
+        self.assertEqual(previous_date.day, (current_date - timedelta(days=1)).day)
