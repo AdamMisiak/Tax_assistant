1ecd520ab0a9ab9c2c02b8dfc846ec5801694bcc
AdamMisiak <adammisiak3@gmail.com>
2022-04-11 17:47:18 +0200

    Added dividends tests

--- tests/test_dividends.py
+++ tests/test_dividends.py
@@ -1,13 +1,8 @@
 from unittest import TestCase
+import dividends
 
-
-# class TestDividends(TestCase):
-#     def test_string(self):
-#         a = 'some'
-#         b = 'sodme'
-#         self.assertEqual(a, b)
-
-#     def test_boolean(self):
-#         a = True
-#         b = True
-#         self.assertEqual(a, b)
+class TestDividends(TestCase):
+    def test_open_csv_file(self):
+        rows = dividends.open_csv_file()
+        self.assertTrue(isinstance(rows, list))
+        self.assertEqual(rows[0][0], 'USD')
