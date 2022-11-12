652ba33e2b0a0be8045bb2e5d24f2bb5f0be8323
AdamMisiak <adammisiak3@gmail.com>
2022-11-12 13:22:00 +0100

    Refactored utils file

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import requests
 import re
 from settings import PLN_CURRENCY
 from os.path import exists
-
+from typing import Optional, Dict, List, Any
 
 def get_csv_file_with_rates(year: str, path: str):
     response = requests.get(
@@ -16,11 +16,11 @@ def get_csv_file_with_rates(year: str, path: str):
         file.close()
 
 
-def get_data_from_csv_file_with_rates(year: str):
+def get_data_from_csv_file_with_rates(year: str) -> List[Dict[str, Any]]:
     rows = []
     with open(f"data/RATES{year}.csv", "r") as file:
         csvreader = csv.reader(file, delimiter=";")
-        for number, row in enumerate(csvreader):
+        for row in csvreader:
             if len(row) == 0 or not re.match(r"^([\d]+)$", row[0]):
                 continue
             result = {
@@ -32,7 +32,7 @@ def get_data_from_csv_file_with_rates(year: str):
                 "EUR": float(row[8].replace(",", ".")),
                 "CHF": float(row[10].replace(",", ".")),
                 "GBP": float(row[11].replace(",", ".")),
-                "RUB": float(row[30].replace(",", ".")),
+                # "RUB": float(row[30].replace(",", ".")),
                 "CNY": float(row[34].replace(",", ".")),
             }
             rows.append(result)
@@ -41,9 +41,8 @@ def get_data_from_csv_file_with_rates(year: str):
 
 def get_previous_day_from_date(date: datetime) -> datetime:
     if isinstance(date, datetime):
-        result = date - timedelta(days=1)
-        return result
-
+        return date - timedelta(days=1)
+    return date
 
 def get_currency_rate_for_date(currency: str, date: datetime) -> float:
     if currency == PLN_CURRENCY:
@@ -51,21 +50,22 @@ def get_currency_rate_for_date(currency: str, date: datetime) -> float:
     start_year = date.year
     date_str_format = date.strftime("%Y%m%d")
     rates = get_data_from_csv_file_with_rates(date.year)
-    index_of_proper_date = next(
-        (index for (index, row) in enumerate(rates) if row["date"] == date_str_format),
-        None,
-    )
-    while index_of_proper_date is None:
+    index_of_correct_date = get_index_of_correct_date(rates, date_str_format)
+ 
+    while index_of_correct_date is None:
         date -= timedelta(days=1)
         date_str_format = date.strftime("%Y%m%d")
         if date.year != start_year:
             rates = get_data_from_csv_file_with_rates(date.year)
-        index_of_proper_date = next(
-            (
-                index
-                for (index, row) in enumerate(rates)
-                if row["date"] == date_str_format
-            ),
-            None,
-        )
-    return rates[index_of_proper_date][currency]
+        index_of_correct_date = get_index_of_correct_date(rates, date_str_format)
+    return rates[index_of_correct_date][currency]
+
+def get_index_of_correct_date(rates: List[Dict[str, Any]], date_str_format: str) -> Optional[int]:
+    return next(
+        (
+            index
+            for (index, row) in enumerate(rates)
+            if row["date"] == date_str_format
+        ),
+        None
+    )
