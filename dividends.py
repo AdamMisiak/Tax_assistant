d146ddba1bc59d69f889a67d2641a6230f83658a
AdamMisiak <adammisiak3@gmail.com>
2022-11-21 21:28:28 +0100

    Added dotenv lib + testing writing data to gsheet

--- dividends.py
+++ dividends.py
@@ -1,13 +1,25 @@
 # pylint: disable=W0640
 # cell-var-from-loop
 import csv
+import json
+import os
 from datetime import datetime
 from typing import Any, Dict, List, Tuple
 
+from dotenv import load_dotenv
+
 import settings
+from gsheet import GoogleWorkbook
 from utils import get_currency_rate_for_date, get_previous_day_from_date
 
 # get data from: https://nbp.pl/kursy/Archiwum/archiwum_tab_a_2021.xls
+
+load_dotenv()
+
+with open("credentials.json") as json_file:
+    credentials_json = json.load(json_file)
+
+# TODO create class here for divs only
 
 
 def get_summary_dividends_tax() -> float:
@@ -21,7 +33,6 @@ def open_csv_file():
     rows = []
     with open(file=settings.DIVIDEND_FILE_CSV, mode="r", encoding="utf-8") as file:
         csvreader = csv.reader(file)
-        next(csvreader)
         for row in csvreader:
             rows.append(row[0].replace('"', "").split("|"))
         return rows
@@ -31,7 +42,6 @@ def get_relevant_data_from_report(report: list) -> Tuple[List[Dict[str, Any]], L
     dividends_report = []
     taxes_report = []
     for row in report:
-        print(row)
         if row[5] in ["Dividends"]:
             record = {}
             record["name"] = row[1]
@@ -72,6 +82,9 @@ def calculate_tax_to_pay(dividends_report: List[Dict[str, Any]], taxes_report: L
             {"value_pln": 0},
         )
 
+        save_record_to_gsheet(received_dividend)
+        break
+
         # if received_dividend["currency"] != settings.PLN_CURRENCY:
         if received_dividend["name"] in settings.MLP_STOCKS:
             # 37% + 4%
@@ -84,3 +97,18 @@ def calculate_tax_to_pay(dividends_report: List[Dict[str, Any]], taxes_report: L
         total_tax_to_paid_in_pln += tax_to_paid_in_pln
 
     return round(total_tax_to_paid_in_pln, 2)
+
+
+def save_record_to_gsheet(received_dividend):
+    print(received_dividend)
+
+
+# google_workbook = GoogleWorkbook(credentials_json=credentials_json, sheet_url="os.getenv('google_sheet_url')")
+# sheet = google_workbook.pull_sheet("Div History")
+# for row in sheet.sheet_data:
+#     print(row)
+
+# sheet.batch_add_single_cell(
+#     index=160+2, column="Comments", value="TEST"
+# )
+# sheet.execute_batch()
--- summary.py
+++ summary.py
@@ -6,23 +6,14 @@ from options import get_summary_options_tax
 from stocks import get_summary_stocks_tax
 from utils import get_csv_file_with_rates
 
-with open("credentials.json") as json_file:
-    credentials_json = json.load(json_file)
-
-# TODO add envs for links and credentials
-google_workbook = GoogleWorkbook(credentials_json=credentials_json, sheet_url="")
-sheet = google_workbook.pull_sheet("Div History")
-print(sheet.sheet_data)
-
-
-# total_tax_to_paid_in_pln_dividends = get_summary_dividends_tax()
+total_tax_to_paid_in_pln_dividends = get_summary_dividends_tax()
 # # total_tax_to_paid_in_pln_stocks = get_summary_stocks_tax()
 # # total_tax_to_paid_in_pln_options = get_summary_options_tax()
 
-# print("SUMMARY:")
-# print("---" * 20)
-# print(f"DIV: {total_tax_to_paid_in_pln_dividends} PLN")
-# print("---" * 20)
+print("SUMMARY:")
+print("---" * 20)
+print(f"DIV: {total_tax_to_paid_in_pln_dividends} PLN")
+print("---" * 20)
 # # print(f"STK: {total_tax_to_paid_in_pln_stocks} PLN")
 # # print("---" * 20)
 # # print(f"OPT: {total_tax_to_paid_in_pln_options} PLN")
