5f6df2fcf24e1dcf3544751592e342098184dce4
AdamMisiak <adammisiak3@gmail.com>
2022-12-08 16:15:28 +0100

    Added few methods to StocksHandler

--- summary.py
+++ summary.py
@@ -2,19 +2,21 @@ import json
 
 from dividends import DividendsHandler
 from options import OptionsHandler
-from stocks import get_summary_stocks_tax
+from stocks import StocksHandler
 from utils import get_csv_file_with_rates
 
-dividend_handler = DividendsHandler()
-option_handler = OptionsHandler()
-total_dividends_tax_to_paid_in_pln = dividend_handler.calculate_tax_to_pay()
-total_options_tax_to_paid_in_pln = option_handler.calculate_tax_to_pay()
+stocks_handler = StocksHandler()
+dividends_handler = DividendsHandler()
+options_handler = OptionsHandler()
+total_stocks_tax_to_paid_in_pln = stocks_handler.calculate_tax_to_pay()
+total_dividends_tax_to_paid_in_pln = dividends_handler.calculate_tax_to_pay()
+total_options_tax_to_paid_in_pln = options_handler.calculate_tax_to_pay()
 
 print("SUMMARY:")
 # print("---" * 20)
 print(f"DIV: {total_dividends_tax_to_paid_in_pln} PLN")
 # print("---" * 20)
-# # print(f"STK: {total_tax_to_paid_in_pln_stocks} PLN")
+print(f"STK: {total_stocks_tax_to_paid_in_pln} PLN")
 # # print("---" * 20)
 print(f"OPT: {total_options_tax_to_paid_in_pln} PLN")
 # # print("---" * 20)
